cadf829a4c74b18438ed12d39d9609f1787472a2
Nikhil Maserang <nmaserang@berkeley.edu>
2022-09-09 19:27:28 -0700

    Updated readme and added some prototype code for finding the Wigner-Seitz cell.

--- README.md
+++ README.md
@@ -30,11 +30,38 @@ The user can toggle gridlines on/off and set the viewing region (axis bounds).
 The user can left-click on lattice points to toggle highlighting them and displaying their lattice coordinates. The user can right-click on lattice points to toggle a vector decomposition for the point, which shows how the basis vectors can be added to obtain that point.
 
 ## Wigner-Seitz Cell
-The user can step through the steps to find the Wigner-Seitz cell for a lattice. The steps are explained below:
-TODO
+The Wigner-Seitz cell is defined as the set of points which are closer to the lattice point $00$ than they are to any other lattice point. This set is a primitive unit cell because it contains just 1 lattice point ($00$) and can completely tile the lattice plane with no gaps nor overlaps. There is a very simple construction which can be used to find the vertices of this cell, which is explained below.
+
+To tackle this problem, we first want to break it up into smaller manageable pieces. So, suppose we have two points: $00$, and some other point $P$. How can we find all points which are closer to the origin than they are to $P$? The answer is as follows:
+1. Draw the line between the origin and $P$.
+2. Draw the perpendicular bisector of this line.
+3. All of the points which are on the origin's side of the line are closer to the origin, while all points on $P$'s side of the line are closer to $P$. This might seem obvious, but it will be very helpful when we scale up our problem.
+
+Suppose now that we have three points: the origin, some point $P$, and another point $Q$. If we want to find all the points that are closer to the origin than they are to either $P$ *or* $Q$, we will basically do the same procedure we did above. We'll first find the set of points which are closer to the origin than $P$, and then we'll find the set of points that are closer to the origin than $Q$. Then, we'll take the intersection of both sets to get the points that are closer to the origin than either $P$ or $Q$.
+
+Our lattice has an infinite number of sets, but we want a finite number of sets so we can carry out our procedure. Therefore, we need to find some set of lattice points that are "representative" of all of the lattice points, in a certain manner of speaking.
+
+To do this, we will notice that if we have the point $P = m_1a + n_1b$, where $a$ and $b$ are lattice vectors and $m_1$ and $n_1$ are scalars, we do not also need any other point $Q = m_2a + n_2b$, where $m_2 > m_1$ and $n_2 > n_1$, because all the points closer to the origin than $P$ are also closer to the origin than $Q$, but some of the points closer to the origin than $Q$ are not closer to the origin than $P$. Therefore, $P$ is "more useful" in some sense for our purposes.
+
+From this, we can see that we just need to check the lattice points that are closest to the origin. These eight points are:
+- $\overline{1}\overline{1}$
+- $0\overline{1}$
+- $1\overline{1}$
+- $10$
+- $11$
+- $01$
+- $\overline{1}1$
+- $\overline{1}0$
+
+If we find the sets of points that are closer to the origin than to each of these points respectively, and then take the intersection, we get our desired set of points.
+
+To get the vertices of this polygon, we'll just pairwise intersect the bisectors each of our points generated.
+
+The user can generate and plot the Wigner-Seitz cell for any lattice, as well as find its area.
 
 ## Future Features
 - Custom motif at each lattice point
 - Lattice transformations
 - Lattice symmetries
 - Loading/saving views
+- Area of standard primitive lattice cells
--- app.py
+++ app.py
@@ -61,6 +61,7 @@ class Lattice2D:
         Points whose lattice coordinate representation consists of only integers are considered
         valid lattice points.
         """
+        # Safety first!
         assert len(basis) == 2, f"{len(basis)} basis vectors provided, while 2 required."
         for vector in basis:
             assert(len(vector)) == 2, f"Length of basis vector {vector} must be 2. Was {len(vector)}"
@@ -90,9 +91,113 @@ class Lattice2D:
         # if a number equals its floor, it's an integer
         lat_points = np.array(self.trans_std_to_lat(std_points))
         return [all(point) for point in lat_points == np.floor(lat_points)]
+
+    def get_
+
+    def generate_wigner_seitz_lines(u : tuple[float, float], v : tuple[float, float]) -> list[tuple[tuple[float, float], tuple[float, float]]]:
+        """
+        Given the two basis vectors u and v of the lattice, returns a list of line segments which
+        border the Wigner Seitz cell.
+        """
+        P_N_to_S = lambda point: transform_point_N_to_S(u, v, point)
+
+        # first get the 8 lattice points closest to 00, in clockwise order from -1 -1
+        x, y = [-1, -1, -1, 0, 1, 1, 1, 0], [-1, 0, 1, 1, 1, 0, -1, -1]
+        transformed_x, transformed_y = [], []
+
+        # iterate through the transformation of the cartesian product of {-1, 0, 1} with itself
+        for point in zip(x, y):
+            x1, y1 = P_N_to_S(point)
+            transformed_x.append(x1)
+            transformed_y.append(y1)
+
+        # since the Wigner Seitz cell is guaranteed to be a hexagon for 2D nonrectangular lattices,
+        # we only need 6 lines to define it. in the case of a rectangular lattice, we only need 4, and
+        # the other four have redundant information. however, in the case of the hexagon, two of the
+        # lines do not contribute to the hexagon, making processing them to find the vertices (and hence
+        # the area) difficult. therefore, we want to remove them. looking at many examples, we see that
+        # the lines created based on the two lattice points furthest from the origin are the problematic
+        # ones. therefore, we will remove these two lattice points from our list
+        for _ in range(2):
+            max_distance = 0
+            max_index = 0
+            for i in range(len(transformed_x)):
+                x1, y1 = transformed_x[i], transformed_y[i]
+                d = np.sqrt(x1 ** 2 + y1 ** 2)
+                if d > max_distance:
+                    max_distance = d
+                    max_index = i
+            transformed_x.pop(max_index)
+            transformed_y.pop(max_index)
+
+        # each line will be a 2-tuple containing two 2-tuples, one for the x points and one for the y
+        lines = []
+
+        # cutting each lattice point in 2 gives the bisecting point. adding (-b, a) and (b, -a) to this
+        # point, where (a, b) is one of the lattice points, gives endpoints for a line guaranteed to be
+        # longer than the edge of the Wigner Seitz cell which it borders
+        for a, b in zip(transformed_x, transformed_y):
+            P1 = (a / 2 - b, b / 2 + a)
+            P2 = (a / 2 + b, b / 2 - a)
+            line = ((P1[0], P2[0]), (P1[1], P2[1]))
+            lines.append(line)
+
+        return lines
+
+    def get_wigner_seitz_vertices(lines : list[tuple[tuple[float, float], tuple[float, float]]]) -> list[tuple[float, float]]:
+        """
+        Given a list of lines describing a Wigner-Seitz cell, returns the vertices of the cell. Assumes
+        that the lines are given in sequential order, because it finds the vertices by finding the
+        intersections of the lines. Uses code from https://stackoverflow.com/a/3252222
+        """
+        def perp(a) :
+            b = np.empty_like(a)
+            b[0] = -a[1]
+            b[1] = a[0]
+            return b
+
+        def seg_intersect(a1, a2, b1, b2) :
+            da = a2-a1
+            db = b2-b1
+            dp = a1-b1
+            dap = perp(da)
+            denom = np.dot( dap, db)
+            num = np.dot( dap, dp )
+            return (num / denom.astype(float))*db + b1
+
+        vertices = []
+
+        # intersect each line with the previous one, so 0 with -1, 1 with 0, etc to n - 1 with n - 2,
+        # where n is the number of lines
+        for i in range(len(lines)):
+            a = lines[i - 1]
+            b = lines[i]
+            a1 = np.array((a[0][0], a[1][0]))
+            a2 = np.array((a[0][1], a[1][1]))
+            b1 = np.array((b[0][0], b[1][0]))
+            b2 = np.array((b[0][1], b[1][1]))
+            vertices.append(seg_intersect(a1, a2, b1, b2))
+
+        # remove duplicate points in the case of a rectangular WS cell, from https://stackoverflow.com/a/44628266
+        lookup = set()
+        #vertices = [vertex for vertex in vertices if vertex not in lookup and lookup.add(vertex) is None]
+
+        # split into x and y lists
+        vx, vy = [], []
+        for vertex in vertices:
+            vx.append(vertex[0])
+            vy.append(vertex[1])
+        return vx, vy
     
-    def validate_basis(self, other_basis):
-        raise NotImplementedError()
+class LatticePlot:
+    def __init__():
+        pass
+
+
+
+class AxesManager:
+    def __init__():
+        pass
 
 def main():
     # initialize window
